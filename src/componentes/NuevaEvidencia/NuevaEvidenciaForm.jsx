eee4f64bb667a8d726421fad86d882945632d0cd
Alejandro Bermúdez <2060559@alu.murciaeduca.es>
2026-01-31 20:45:57 +0100

    añadidos campos url y observaciones (sin funciones aún)

--- src/componentes/NuevaEvidencia/NuevaEvidenciaForm.jsx
+++ src/componentes/NuevaEvidencia/NuevaEvidenciaForm.jsx
@@ -1,28 +1,43 @@
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
+import "./Evidencias.css"
 
 const NuevaEvidenciaForm = (props) => {
 
 	function añadirEvidencia() {
 
-        return <></>
-    }
+		return <></>
+	}
 
 
-    function borrarTareas() {
-        props.setListaTareas([])
-        setTareaSeleccionada("")
-    }
+	function borrarTareas() {
+		props.setListaTareas([])
+		setTareaSeleccionada("")
+	}
 
 	return (
-		<>
+		<div className='ancho'>
 			<h2>Formulario Tarea</h2>
 			<p>Tarea seleccionada: {props.tareaSeleccionada.id} - {props.tareaSeleccionada.observaciones}</p>
+			<TextField 
+				id="outlined-basic" 
+				label="URL" 
+				variant="outlined" 
+			/>
+			<br />
+			<br />
+			<TextField
+				id="outlined-textarea"
+				label="Observaciones"
+				placeholder="Observaciones"
+				multiline
+			/>
 			<Stack direction="row" spacing={2}>
 				<Button variant="contained" onClick={añadirEvidencia}>Añadir evidencia</Button>
 				<Button variant="contained" onClick={borrarTareas}>Borrar tareas</Button>
 			</Stack>
-		</>
+		</div>
 	)
 }
 
