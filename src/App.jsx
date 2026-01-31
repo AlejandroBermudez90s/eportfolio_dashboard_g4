5ad452badf24b10f6f98735114dfb095416fdd56
Alejandro Bermúdez <2060559@alu.murciaeduca.es>
2026-01-31 11:08:44 +0100

    usuario con useState

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Cabecera from './componentes/Cabecera.jsx'
 import Roles from './componentes/Roles.jsx'
 import Dashboard from './componentes/Dashboard.jsx'
 import TokenContext from './componentes/TokenContext.jsx'
+import { useState } from 'react'
 
 function App() {
 
@@ -10,12 +11,14 @@ function App() {
   const token = 'abc123xyz456'
   const menu = 'Principal'
 
+  const [user, setUser] = useState(usuario)
+
   return (
     <TokenContext.Provider value = {token}>
       <div className='container-fluid'>
         <div className='row'>
           <div className='col-12'>
-            <Cabecera usuario={usuario}></Cabecera>
+            <Cabecera usuario={user}></Cabecera>
           </div>
         </div>
         <div className='row'>
