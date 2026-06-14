8a79bdc6d4b5ef05b956a332876878b003aef36c
Alejandro Bermúdez <2060559@alu.murciaeduca.es>
2026-06-14 17:02:33 +0200

    errata corregida

--- src/componentes/Layout/Layout.jsx
+++ src/componentes/Layout/Layout.jsx
@@ -3,7 +3,7 @@ import Dashboard from "../Dashboard/Dashboard"
 import { Routes, Route } from 'react-router-dom'
 import FuncionalidadEstudiante from "../FuncionalidadEstudiante/FuncionalidadEstudiante"
 import FuncionalidadDocente from "../FuncionalidadDocente/FuncionalidadDocente"
-import FamiliasProfesionales from "../FamiliasProfesionales/PaginaFamiliasProfesionales";
+import FamiliasProfesionales from "../FamiliasProfesionales/FamiliasProfesionales";
 
 const Layout = (props) => {
 
